62345ddccc9154e8e91bcd39c78055f0770b827a
Anatolii <Anatolii_lukin@mail.ru>
2022-08-07 01:03:43 +0300

    Add font to jasper report

--- src/main/java/com/mycompany/mywebapp/jasperreport/ReportService.java
+++ src/main/java/com/mycompany/mywebapp/jasperreport/ReportService.java
@@ -4,12 +4,13 @@ import com.mycompany.mywebapp.entity.Employee;
 import com.mycompany.mywebapp.repository.EmployeeRepository;
 import com.mycompany.mywebapp.repository.ProtocolRepository;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRHibernateAbstractDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ReportService {
@@ -26,14 +27,36 @@ public class ReportService {
             String reportPath = "C:\\Users\\Anato\\Desktop\\MyWebApp\\src\\main\\resources";
 
             // Get your data source
-            Collection<Employee> employees = (List<Employee>) employeeRepository.findAll();
+//            List<Employee> list = (List<Employee>) employeeRepository.findAll();
+//            Optional<Employee> employee = employeeRepository.findById(1l);
+//            Employee employee1 = employee.get();
+//            list.add(employee1);
+//            for(Employee employee6 : employeeRepository.findAll() ){
+//                Employee employee4 = new Employee();
+//                employee4.setId(employee6.getId());
+//                employee4.setFirstName(employee6.getFirstName());
+//                employee4.setLastName(employee6.getLastName());
+//                employee4.setPatronymic(employee6.getPatronymic());
+//                list.add(employee4);
+//            }
+            List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+            for (Employee employee : employeeRepository.findAll()) {
+                Map<String, Object> item = new HashMap<String, Object>();
+                item.put("id", employee.getId());
+                item.put("firstName", employee.getFirstName());
+                item.put("lastName", employee.getLastName());
+                item.put("patronymic", employee.getPatronymic());
+                result.add(item);
+            }
 
-            JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(employees);
+
+            JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(result);
+
 
             // Compile the Jasper report from .jrxml to .japser
             JasperReport jasperReport = JasperCompileManager
-                    .compileReport(reportPath + "\\Simple_Blue.jrxml");
-//                    .compileReport(reportPath + "\\V3.jrxml");
+//                    .compileReport(reportPath + "\\Simple_Blue.jrxml");
+                    .compileReport(reportPath + "\\Protocol.jrxml");
 
             // Fill the report
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
